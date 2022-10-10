2f5e02286b40d464afe9a67de4fbc21077a8140c
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2022-10-10 10:47:27 +0100

    Add files via upload

---
+++ Arrays and Lists/Exercise 1/Arrays and Lists 1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var names = new List<string>();
+
+while (true)
+{
+    Console.WriteLine("Please add the name of a friend or press ENTER to quit.");
+    var name = Console.ReadLine();
+    if (String.IsNullOrEmpty(name))
+    {
+        break;
+    }
+    else
+        names.Add(name);
+}
+
+if (names.Count == 0)
+    Console.WriteLine();
+else if (names.Count == 1)
+    Console.WriteLine(names[0] + " liked your post");
+else if (names.Count == 2)
+    Console.WriteLine(names[0] + " and " + names[1] + " liked your post");
+else
+    Console.WriteLine(names[0] + ", " + names[1] + " and " + (names.Count - 2) + " others liked your post");
+
+//Console.WriteLine(names);
