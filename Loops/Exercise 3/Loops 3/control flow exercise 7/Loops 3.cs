4cb4bd7e31a868625546ef2919b78f5fa9d2792f
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2022-10-10 10:20:40 +0100

    Add files via upload

---
+++ Loops/Exercise 3/Loops 3/control flow exercise 7/Loops 3.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number to calculate its factorial");
+int number = int.Parse(Console.ReadLine());
+int i,factorial=1;
+for (i = 1; i <= number; i++)
+    factorial = factorial * i;
+Console.WriteLine("The factorial of " + number + " is " + factorial);
