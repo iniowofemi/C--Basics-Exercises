8359154663dd43951820d8510db77ba8b333ad97
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2022-10-10 10:51:14 +0100

    Add files via upload

---
+++ Working with Text Exercises/Exercise 3/Working with Text 3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*  Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00).
+ *  A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok";
+ *  otherwise, display "Invalid Time". If the user doesn't provide any values, consider it as invalid time.
+ */
+Console.WriteLine("Write a time value in 24 hour format");
+var input = Console.ReadLine().Split(':');
+
+var time = new List<int>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    time.Add(Convert.ToInt32(input[i]));
+}
+
+if (time[0] > 23 || time[1] > 59)
+{
+    Console.WriteLine("Invalid Time");
+}
+else
+{
+    Console.WriteLine("Ok");
+}
