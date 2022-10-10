353c2723f105e35a6ded9524c51093b11ed85711
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2022-10-10 10:21:03 +0100

    Add files via upload

---
+++ Loops/Exercise 4/Loops 4/control flow exercise 8/Loops 4.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+var random = new Random();
+int numberToGuess = random.Next(1, 10);
+Console.WriteLine(numberToGuess); //check what the number generated is to test code easily
+for (var i = 0; i < 4; i++)
+{
+    Console.WriteLine("Guess a a number between 1-10 inclusive");
+    int guess = Int32.Parse(Console.ReadLine());
+    if (guess == numberToGuess)
+        i = 6;
+    Console.WriteLine("Well done! you guessed correctly");
+    else if (i == 3)
+        Console.WriteLine("You lost!");
+    else
+        Console.WriteLine("Try again!");
+
+}
