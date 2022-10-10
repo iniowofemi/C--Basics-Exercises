0e96c131fa8a3979c3a3ec00f876766536651aa2
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2022-10-10 10:20:35 +0100

    Add files via upload

---
+++ Loops/Exercise 2/Loops 2/control flow exercise 6/Loops 2.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+var total = 0;
+while (true)
+{
+    Console.WriteLine("Enter a number or 'ok' to add them all together");
+    var input = Console.ReadLine();
+    if (input == "ok")
+        break;
+    total += Int32.Parse(input);
+    Console.WriteLine("Current total is: " + total);
+}
