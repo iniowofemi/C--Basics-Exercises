0690bd8b264e0da8decf0d32c04ec17f3bc15063
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2022-10-10 10:17:26 +0100

    Add files via upload

---
+++ Conditionals/Exercise 3/Conditionals 3/control flow exercise 3/Conditionals 3.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the width of the image in metres");
+string input1 = Console.ReadLine();
+int width = Int32.Parse(input1);
+
+Console.WriteLine("Enter the height of the image in metres");
+string input2 = Console.ReadLine();
+int height = Int32.Parse(input2);
+
+if (width > height)
+    Console.WriteLine("This image is landscape");
+else if (width < height)
+    Console.WriteLine("This image is portrait");
+else
+    Console.WriteLine("This image is a square");
