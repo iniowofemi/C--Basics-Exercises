94fdc7c427942d50127208bfafc9aa9b355a4dfa
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2022-10-10 10:17:07 +0100

    Add files via upload

---
+++ Conditionals/Exercise 2/Conditionals 2/control flow exercise 2/Conditionals 2.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number");
+string input1 = Console.ReadLine();
+int number1 = Int32.Parse(input1);
+
+Console.WriteLine("Enter a second number");
+string input2 = Console.ReadLine();
+int number2 = Int32.Parse(input2);
+
+if (number1 > number2)
+    Console.WriteLine(number1);
+else if (number1 < number2)
+    Console.WriteLine(number2);
+else
+    Console.WriteLine("They are the same");
