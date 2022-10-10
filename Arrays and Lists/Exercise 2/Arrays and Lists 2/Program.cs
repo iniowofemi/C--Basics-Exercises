9431e85c6863959bdb04e0d7be3d9b1afe052b3f
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2022-10-10 10:47:34 +0100

    Add files via upload

---
+++ Arrays and Lists/Exercise 2/Arrays and Lists 2/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Write a program to take a name input, reverse it, and display it on the console
+
+Console.WriteLine("Please enter your name:");
+var input = Console.ReadLine();
+var nameArray = input.ToCharArray();
+
+//for (int i = 0; i < nameArray.Length; i++)
+//{
+//    Console.WriteLine(nameArray[i]);
+//}
+
+Array.Reverse(nameArray);
+Console.WriteLine("The reversed name is:");
+foreach (var n in nameArray)
+    Console.WriteLine(n);
