37a5efc027c80607be11dac03f083cfe7c3ea695
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2022-10-10 10:51:03 +0100

    Add files via upload

---
+++ Working with Text Exercises/Exercise 2/Working with Text 2/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Write a program and ask the user to enter a few numbers separated by a hyphen.
+ * If the user simply presses Enter, without supplying an input, exit immediately;
+ * otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.
+ */
+Console.WriteLine("Please enter a series of numbers separated by a hyphen (e.g. 2-4-6-7");
+var input = Console.ReadLine();
+
+if (!String.IsNullOrEmpty(input))
+{
+    var numbers = input.Split('-');
+    Array.Sort(numbers);
+
+   
+    for (int i = 1; i < numbers.Length; i++)
+    {
+        if (numbers[i] == numbers[i - 1])
+        {
+            Console.WriteLine("Duplicate exists");
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Have a good day");
+}
