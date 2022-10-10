3fb41fa96aa3c4ac777ae364aa48c85cc42d074b
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2022-10-10 10:51:02 +0100

    Add files via upload

---
+++ Working with Text Exercises/Exercise 1/Working with Text 1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Write a program and ask the user to enter a few numbers separated by a hyphen.
+ * Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16",
+ * display a message: "Consecutive"; otherwise, display "Not Consecutive".
+ */
+Console.WriteLine("Please enter a series of numbers separated by a hyphen (e.g. 2-4-6-7");
+var input = Console.ReadLine();
+var numbers = input.Split('-');
+
+var intList = new List<int>();
+foreach (var number in numbers)
+{
+    intList.Add(Convert.ToInt32(number));
+}
+
+var isConsecutive = true;
+for (var i = 1; i < intList.Count; i++)
+{
+    if ((intList[i] - intList[i - 1] != 1) || (intList[i] - intList[i - 1] != -1))
+    {
+        isConsecutive = false;
+        break;
+    }
+}
+
+var message = isConsecutive ? "Consecutive" : "Not Consecutive";
+Console.WriteLine(message);
