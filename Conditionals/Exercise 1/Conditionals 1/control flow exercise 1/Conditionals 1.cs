84ec7a7ddf340ae41f361977aecc0b2ac2d002e9
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2022-10-10 10:17:17 +0100

    Add files via upload

---
+++ Conditionals/Exercise 1/Conditionals 1/control flow exercise 1/Conditionals 1.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number between 1 and 10 exclusive:");
+string input = Console.ReadLine();
+int number = Int32.Parse(input);
+if (number > 0 && number < 11)
+    Console.WriteLine("Valid");
+else
+    Console.WriteLine("Invalid");
